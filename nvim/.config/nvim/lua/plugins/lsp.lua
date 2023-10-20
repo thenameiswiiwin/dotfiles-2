c18a2c4ddaadfe23762ac2cf0d550829ab61e87d
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-10-20 13:33:20 -0400

    update

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "gK", vim.lsp.buf.hover }
     end
   },
   {
